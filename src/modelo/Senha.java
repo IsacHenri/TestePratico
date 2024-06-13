a8c47272ca14f1c0d3ab90a287c3d7552ea1642a
Isac Henrique da Cunha <134301248+IsacHenri@users.noreply.github.com>
2024-06-13 09:07:32 -0300

    Melhorando comentarios Senha.java

--- src/modelo/Senha.java
+++ src/modelo/Senha.java
@@ -4,7 +4,7 @@ public class Senha {
 	private String conteudo;
 
 	
-	
+	//contrutor verificando se a senha que esta sendo passada é maior que 10 e menor que 30 e se possui o caracter maiusculo "A"
 	public Senha(String conteudo) {
 		super();
 		if(conteudo.length() > 10) {
@@ -21,6 +21,8 @@ public class Senha {
 		return conteudo;
 	}
 
+	// verificando se na hora da alteração da senha, está sendo passado mais de 10 e menos de 30, verifica se contem a letra maiuscula "A" e "B", verifica se possui a letra minuscula "a",
+	// verifica se possui caracter especial "@", verifica se possui algum numero "1" para salvar a senha se não salva como: "Nao foi possivel alterar, precisa de uma senha mais forte".
 	public void setConteudo(String conteudo) {
 		if(conteudo.length() > 10) {
 			if(conteudo.length() < 30) {
@@ -43,7 +45,8 @@ public class Senha {
 		}
 		
 	}
-	
+
+	// metodo que verifica se a senha é forte e retorna os pontos pra saber o nivel de segurança
 	public int forcaSenha() {
 		if(conteudo.length() < 10) {
 			return 0;
@@ -61,6 +64,7 @@ public class Senha {
 		
 	}
 
+	//metodo to string para poder exibir a classe
 	@Override
 	public String toString() {
 		return "Senha [conteudo=" + conteudo + "]";
